a79fd439c7332b2936cbd657a9423813c343cef5
Rounak Bhowmick <rounakchakda@gmail.com>
2020-06-30 16:34:47 +0530

    completed

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,4 +1,40 @@
 package utility;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class ConnectionManager {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Properties prop = null;
+		try {
+			prop = loadPropertiesFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		final String driver = prop.getProperty("driver");
+		final String url = prop.getProperty("url");
+		final String username = prop.getProperty("username");
+		final String password = prop.getProperty("password");
+		Class.forName(driver);
+		Connection con = null;
+		con = DriverManager.getConnection(url, username, password);
+		if (con != null) {
+			System.out.println("Connection establishedd");
+		} else {
+			System.out.println("Check your connection");
+		}
+		return con;
+	}
 
+	public static Properties loadPropertiesFile() throws IOException {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	}
+}
